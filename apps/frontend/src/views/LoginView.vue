47d5e96a3f38c0ef61bb1cbb70f55455ae9b6294
Anhar Hadhitya <67372139+anhar3004@users.noreply.github.com>
2026-04-23 03:11:24 +0700

    Update LoginView.vue

--- apps/frontend/src/views/LoginView.vue
+++ apps/frontend/src/views/LoginView.vue
@@ -71,8 +71,7 @@ const handleLogin = async () => {
   errorMessage.value = '';
 
   try {
-    const response = await fetch('${import.meta.env.VITE_API_URL}/api/login', {
-
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/api/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
