58a1a15cc6ed826cbf08cdc2d9f9a1530922b9c6
Anhar Hadhitya <67372139+anhar3004@users.noreply.github.com>
2026-04-23 03:30:54 +0700

    Update image.ts

--- apps/frontend/src/utils/image.ts
+++ apps/frontend/src/utils/image.ts
@@ -1,10 +1,9 @@
 export const getImageUrl = (path: string | null) => {
   if (!path) return 'https://placehold.co/600x400?text=No+Image';
-
-  // Jika sudah berupa URL (seperti link placeholder dari seeder), langsung return
+  
   if (path.startsWith('http')) return path;
 
-  // Jika hanya nama file (misal: "mixer-6kg.png"), arahkan ke folder public backend
-  const baseUrl = '${import.meta.env.VITE_API_URL}/public/uploads';
+  const baseUrl = `${import.meta.env.VITE_API_URL}/public/uploads`;
+  
   return `${baseUrl}/${path}`;
 };
